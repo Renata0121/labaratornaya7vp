ace9696cb66dde919531cd77353918d8f0d5fdaa
Renata0121 <54933531+Renata0121@users.noreply.github.com>
2019-09-22 16:53:06 +0300

    Add files via upload

---
+++ лаба7задание1вп.cpp
@@ -0,0 +1,20 @@
+﻿// лабазадание1вп.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b;
+	cout << "Введите a, b";
+	cin >> a >> b;
+	if ((a > 2) && (b <= 3)) {
+		cout << "Истинно";
+	}
+	else {
+		cout << "Ложно";
+	}return 0;
+}
